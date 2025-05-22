9771e306138394b65da8a16fe55644784c4f32a5
michaelonskis1 <michaelonskis@gmail.com>
2025-05-21 23:52:54 -0600

    Fix index.html script path to relative

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-site/",  // Replace with your GitHub repo name, including slashes!
+ // base: "/portfolio-site/",  // Replace with your GitHub repo name, including slashes!
 });
